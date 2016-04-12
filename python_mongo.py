94fe3cc2885b9af337cd108515e542bb4c8b1aea
diegocambuy <diego.ddc@gmail.com>
2016-04-12 16:42:28 +0200

    Added files via upload

---
+++ python_mongo.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+client = MongoClient()
+db=client.PIK3CA
+cursor=db.vcf.find()
+n=0
+for i in cursor:
+   if "ENST00000263967" in (i.values()[0])[u'ann']:
+     n=n+1
+
+print n
+
+
+cursor=db.vcf.find()
+n=0
+for i in cursor:
+  if (i.values()[0])[u'dp'] > 7:
+    n=n+1
+
+
+print n
+
+
+
+
+
